ceb5614b9ef178e912833af1bba58f555a8214a4
Junior Costa <juniorcoxtaa@gmail.com>
2026-05-20 12:56:32 -0300

    final

--- drizzle/0008_property_image_url_guards.sql
+++ drizzle/0008_property_image_url_guards.sql
@@ -1,10 +1,12 @@
 update properties
 set image = null
-where image like 'data:%';
+where image is not null
+and trim(image) like 'data:%';
 
 update properties
 set images = null
-where images::text like '%data:%';
+where images is not null
+and images::text like '%data:%';
 
 alter table "properties"
   add constraint "properties_image_no_data_url"
